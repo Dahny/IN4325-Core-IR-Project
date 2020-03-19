81b6ac93c345ca05b9dbc7d1d7a163d52825ab9c
Martijn de Heus <mjdeheus@hotmail.com>
2020-03-19 15:40:16 +0100

    Add semantic features file

---
+++ semanticFeatures.py
@@ -0,0 +1,28 @@
+# FEATURES from their features.csv file:
+# {
+#     'sim',
+#     'max', 
+#     'avg', 
+#     'sum', 
+
+#     'remax', 
+#     'reavg',
+#     'resum', 
+#     'resim', 
+
+#     'eavg', 
+#     'emax', 
+#     'esum', 
+#     'esim', 
+
+#     'csim', 
+#     'csum', 
+#     'cavg', 
+#     'cmax'
+# }
+
+def compute_baseline_features(data_table, query_col, table_col):
+    ''' Compute semantic features introduced by the paper '''
+
+
+    return data_table
